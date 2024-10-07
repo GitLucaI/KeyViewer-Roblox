fa376267b8910827a1e5699544a0c89b4d770b6b
GitLucaI <circusbabyyt589@gmail.com>
2024-10-07 19:54:37 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -6,7 +6,7 @@ local UI = {}
 
 -- // StarterGui.KeyLogger \\ --
 UI["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"))
-UI["1"]["Enabled"] = false
+UI["1"]["Enabled"] = true
 UI["1"]["Name"] = [[KeyLogger]]
 UI["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling
 UI["1"]["ResetOnSpawn"] = false
